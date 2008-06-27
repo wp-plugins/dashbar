aea82eab0167f40fe935f8732cb7dc2d652fd0f0
z720 <z720@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-06-27 07:53:01 +0000

    Initial import
    git-svn-id: https://plugins.svn.wordpress.org/dashbar/tags/2.0.1@52133 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ DashBar.php
@@ -0,0 +1,320 @@
+<?php
+/*
+Plugin Name: DashBar
+Plugin URI: http://z720.net/produits/wordpress/dashbar
+Description: Display a Enhanced WordPress.com-like navigation bar for logged users: direct acces to Dashboard, Write, Edit, Awaiting Moderation, Profile...
+Version: 2.0.1
+Author: Sebastien Erard
+Author URI: http://z720.net/
+*/
+
+if(class_exists('DashBar')) {
+  die('You must deactivate previous version of DashBar Plugin for this version to work');
+} else {
+  class DashBar {
+  /* Attributes */  
+  	var $prefixe = 'DashBar';
+  	var $domain = 'DashBar/DashBar';
+  	var $version = '2.0';
+  
+  	var $default = array( 'bgcolor' => '#14568a', 
+  	                      'height' => '11px', 
+  	                      'align' => 'left', 
+                          'color' => '#c3def1', 
+                          'fgcolor' => '#6da6d1', 
+                          'acolor' => '#000000',
+                          'opacity' => '50');
+    var $values = array();
+  	var $user_menu = array();
+  	
+  	var $erreur = false;
+  	var $togglealign = array('right'=>'left', 'left'=>'right', 'bottom' => 'top', 'top' => 'bottom');
+
+  
+  	
+  	function DashBar() {
+// i18n : load texts
+  		load_plugin_textdomain($this->domain);
+// Load options to overwrite defaults
+  		$c = get_option($this->prefixe);
+  		foreach($this->default as $k => $v) {
+  			if(($c === false) or (!isset($c[$k]))) {
+  				$this->values[$k] = $this->default[$k];
+  			} else {
+  				$this->values[$k] = $c[$k];
+  			}
+  		}
+// save options to database or create default for first time
+  		update_option($this->prefixe, $this->values);
+// WP init
+  		add_action('init', array(&$this, 'init'));
+// WP admin hooks
+  		add_action('admin_head', array(&$this, 'admin_page_header'));
+  		add_action('admin_menu', array(&$this, 'admin_pages')); 
+		
+  	}
+  
+    function init() {
+  		get_currentuserinfo();
+  		global $userdata, $user_login, $user_identity;
+  		if($user_login) {
+  			add_filter('wp_footer', array(&$this, 'display_bar'));
+  			add_filter('wp_head', array(&$this, 'display_style'));
+  		}
+    }
+
+    function admin_pages() {
+      add_options_page(__('DashBar', $this->domain), __('DashBar',$this->domain), 'manage_options', basename(__FILE__), array(&$this, 'admin_page'));    
+    }
+  
+  	function display_bar() {
+  	  get_currentuserinfo();
+  	  global $current_user;
+  		echo '<div id="DashBar">'
+  				.'<ul>';
+  	  echo '<li><a href="'.get_option('siteurl').'/wp-admin/profile.php">'.__('My Account', $this->domain).'</a><ul><li><a href="'.get_option('siteurl').'/wp-login.php?action=logout">'.__('Logout',$this->domain).'</a></li></ul></li>'
+  	  		.'<li><a href="'.get_option('siteurl').'/wp-admin/">'.__('Dashboard',$this->domain).'</a>';
+    	if(current_user_can('activate_plugins') || current_user_can('manage_categories') || current_user_can('manage_links') || current_user_can('manage_files') || current_user_can('manage_options')) {
+    	  echo '<ul>';
+    	  if(current_user_can('manage_categories')) {
+    			echo '<li><a href="'.get_option('siteurl').'/wp-admin/categories.php">'.__('Categories',$this->domain).'</a></li>';
+    	  }
+    	  if (current_user_can('manage_links')) {
+    			echo '<li><a href="'.get_option('siteurl').'/wp-admin/link-manager.php">'.__('Links',$this->domain).'</a></li>';
+    	  }
+    	  if (current_user_can('manage_files')) {
+    			echo '<li><a href="'.get_option('siteurl').'/wp-admin/templates.php">'.__('Files',$this->domain).'</a></li>';
+    	  }
+    	  if (current_user_can('manage_options')) {
+    			echo '<li><a href="'.get_option('siteurl').'/wp-admin/options-general.php">'.__('Options',$this->domain).'</a></li>';
+    	  }
+    	  if (current_user_can('activate_plugins')) {
+    			echo '<li><a href="'.get_option('siteurl').'/wp-admin/plugins.php">'.__('Plugins',$this->domain).'</a></li>';
+    	  }
+    	  echo '</ul>';
+    	}
+  	  echo '</li>';
+  	  if (current_user_can('edit_posts')) {
+  		  echo '<li><a href="'.get_option('siteurl').'/wp-admin/post.php">'.__('Write Post',$this->domain).'</a></li>';
+    		rewind_posts();
+    		if(is_single()||is_page()) {
+    			the_post();
+    			global $id, $authordata;
+      		if(($current_user->ID == $authordata->ID) or current_user_can('edit_others_posts')) {
+    	  		echo '<li><a href="'.get_option('siteurl').'/wp-admin/post.php?action=edit&amp;post='.$id.'" title="'.get_the_title().'">'.__('Edit',$this->domain).'</a></li>';
+    	  	}
+    		} elseif(!is_404()) {
+    			if(have_posts()){
+    				echo '<li><a href="'.get_option('siteurl').'/wp-admin/edit.php">'.__('Edit',$this->domain).'</a><ul>';
+    				while(have_posts()) {
+    					the_post();
+    					global $id, $authordata;
+    					if(($current_user->id == $authordata->ID) or current_user_can('edit_others_posts')) {
+    					  echo '<li><a href="'.get_option('siteurl').'/wp-admin/post.php?action=edit&amp;post='.$id.'" title="'.__('Edit :',$this->domain).get_the_title().'">'.get_the_title('','',false).'</a></li>';
+    					}
+    				}
+    				echo '</ul></li>';
+    			}
+    		}
+    	}
+  		if(current_user_can('moderate_comments')) {
+    		global $wpdb;
+    		$awaiting_mod = $wpdb->get_var("SELECT COUNT(*) FROM $wpdb->comments WHERE comment_approved = '0'");
+    		if($awaiting_mod) {
+    			echo '<li><a href="'.get_option('siteurl').'/wp-admin/moderation.php">'.sprintf(__("Awaiting Moderation (%s)",$this->domain), $awaiting_mod).'</a></li>';
+    		}
+    	}
+    	echo '</ul></div>';
+  	}
+  
+  		
+	  function display_style() {
+?>
+<!-- [Begin:DashBar Style and JS] -->
+<style type="text/css">
+#DashBar {position: fixed; top: 0; left: 0;opacity:<?php echo ($this->values['opacity']/100)?>;background: <?php echo $this->values['bgcolor']; ?>;color:<?php echo $this->values['acolor']; ?>;width: 100%;font-family: sans-serif;font-size: <?php echo $this->values['height']; ?>!important;padding:0; margin:0;}
+#DashBar a {display:block;padding:0.4em 1em;margin:0;color: <?php echo $this->values['color']; ?>;text-decoration: none;font-weight:normal;border:none;}
+#DashBar li:hover, #DashBar li.over, #DashBar a:hover {background: <?php echo $this->values['fgcolor']; ?>;color: <?php echo $this->values['acolor']; ?>;}
+#DashBar ul {list-style: none; float:<?php echo $this->values['align']; ?>; margin:0; padding:0;}
+#DashBar li {float:left;text-align:left;}
+#DashBar ul ul {display:none;position:absolute;background:<?php echo $this->values['bgcolor']; ?>;width:100%;left:0;}
+#DashBar ul li.over ul, #DashBar ul li:hover ul {display:block;}
+</style>
+<!--[if lt IE 7]><style type="text/css">#DashBar {position:absolute;filter:alpha(Opacity=<?php echo ($this->values['opacity'])?>);}#DashBar .over {padding-bottom:2em;}</style><![endif]-->
+<script type="text/javascript">
+function checkDashBar() {
+  if(!document.getElementById) {return;} 
+  var Dashbar = document.getElementById('DashBar');
+  if(!DashBar) {
+    var bodies = document.getElementsByTagName('BODY');
+    var wrn = document.createElement('p');
+    wrn.id = 'DashBar';
+    wrn.innerHTML = '<'+'a href="<?php echo get_option('siteurl').'/wp-admin/theme-editor.php'; ?>"'+'>'+'<?php echo addslashes(__('Your theme is not ready for the DashBar Plugin. To use this Plugin you should add &lt;?php wp_footer(); ?&gt; in the footer of your theme.', $this->domain))?>'+'<'+'/'+'a'+'>';
+    bodies[0].appendChild(wrn);
+  } else {
+    if(document.all) {
+      var elts = DashBar.getElementsByTagName('LI');
+      for(i=0;i<elts.length;i++) {
+        if((elts[i].parentNode.parentNode.id == 'DashBar') && (elts[i].childNodes.length > 1)) {
+          elts[i].onmouseover = function() {this.className+= " over";};
+          elts[i].onmouseout = function() {this.className = this.className.replace(" over","");};
+        }
+      }
+    }
+  }
+}
+function addDashBarLoadEvent(func) {if (typeof window.onload != 'function') {window.onload = func;} else {var old = window.onload;window.onload = function() {old();func();}}}
+addDashBarLoadEvent(checkDashBar);
+</script>
+<!-- [End:DashBar Style and JS] -->
+<?php		
+	  }
+ 	
+  // Administration part
+  	function admin_page_header() {
+  		if((isset($_REQUEST['page'])) and ($_REQUEST['page'] == basename(__FILE__))) {
+  			switch($_REQUEST['DashBarAction']) {
+  				case __('Change Style',$this->domain):
+  					if(isset($_REQUEST['DashBarStyle']['bgcolor']) and preg_match('/^#[0-9a-f]{3,6}$/i',$_REQUEST['DashBarStyle']['bgcolor'])) {
+  						$this->values['bgcolor'] = $_REQUEST['DashBarStyle']['bgcolor'];
+  					} else {
+  						$this->erreur[] = __("Incorrect color, it should be in hexadecimal form with 3 or 6 characters",$this->domain);
+  					}
+  					if(isset($_REQUEST['DashBarStyle']['fgcolor']) and preg_match('/^#[0-9a-f]{3,6}$/i',$_REQUEST['DashBarStyle']['fgcolor'])) {
+  						$this->values['fgcolor'] = $_REQUEST['DashBarStyle']['fgcolor'];
+  					} else {
+  						$this->erreur[] = __("Incorrect color, it should be in hexadecimal form with 3 or 6 characters",$this->domain);
+  					}
+  					if(isset($_REQUEST['DashBarStyle']['color']) and preg_match('/^#[0-9a-f]{3,6}$/i',$_REQUEST['DashBarStyle']['color'])) {
+  						$this->values['color'] = $_REQUEST['DashBarStyle']['color'];
+  					} else {
+  						$this->erreur[] = __("Incorrect color, it should be in hexadecimal form with 3 or 6 characters",$this->domain);
+  					}
+  					if(isset($_REQUEST['DashBarStyle']['acolor']) and preg_match('/^#[0-9a-f]{3,6}$/i',$_REQUEST['DashBarStyle']['acolor'])) {
+  						$this->values['acolor'] = $_REQUEST['DashBarStyle']['acolor'];
+  					} else {
+  						$this->erreur[] = __("Incorrect color, it should be in hexadecimal form with 3 or 6 characters",$this->domain);
+  					}
+  					if(isset($_REQUEST['DashBarStyle']['height']) and preg_match('/^[0-9]+(|px|em|ex|%)$/i',$_REQUEST['DashBarStyle']['height'])) {
+  						$this->values['height'] = $_REQUEST['DashBarStyle']['height'];
+  					} else {
+  						$this->erreur[] = __("Incorrect text size format",$this->domain);
+  					}
+  					if(isset($_REQUEST['DashBarStyle']['align']) and preg_match('/^(right|left)$/i',$_REQUEST['DashBarStyle']['align'])) {
+  						$this->values['align'] = $_REQUEST['DashBarStyle']['align'];
+  					} else {
+  						$this->erreur[] = __("You can only align to right or left",$this->domain);
+  					}
+  					if(isset($_REQUEST['DashBarStyle']['opacity']) and preg_match('/^[0-9]{1,3}$/i',$_REQUEST['DashBarStyle']['opacity']) and ($_REQUEST['DashBarStyle']['opacity']<=100)) {
+  						$this->values['opacity'] = $_REQUEST['DashBarStyle']['opacity'];
+  					} else {
+  						$this->erreur[] = __("Opacity must be an integer between 0 and 100",$this->domain);
+  					}
+  					if(!$this->erreur) {
+  						update_option($this->prefixe, $this->values);
+  						$this->msg = __('New settings successfully saved', $this->domain);
+  					}
+  					break;
+  				case __('Restore Default',$this->domain):
+  						update_option($this->prefixe, $this->default);
+  						$this->values = $this->default;
+  						$this->msg = __('Default settings successfully restored', $this->domain);
+  					break;
+  				default:
+  			}
+  			echo '
+  			<script type="text/javascript" src="../wp-includes/js/colorpicker.js"></script>
+  			<script type="text/javascript">
+  			var cp = new ColorPicker();
+  			function initColorPicker() {
+  			  var elts = document.getElementsByTagName(\'INPUT\');
+  			  for(i=0;i<elts.length;i++) {
+  			    if(elts[i].id.substr(elts[i].id.length-7,elts[i].id.length) == \'_sample\') {
+  			      elts[i].onclick = color_picker;
+  			      elts[i].style.backgroundColor = document.getElementById(elts[i].id.replace(\'_sample\',\'\')).value;
+  			    }
+  			  }
+  			}
+  			addLoadEvent(initColorPicker);
+  			function color_picker() {
+  			  var input_id = this.id.replace(\'_sample\',\'\');
+  			  var color_field = document.getElementById(input_id);
+      		if ( cp.p == input_id && document.getElementById(cp.divName).style.visibility != "hidden" )
+      			cp.hidePopup(\'prettyplease\');
+      		else {
+      			cp.p = input_id;
+      			cp.select(color_field,input_id);
+      		}
+      	}
+      	function pickColor(color) {
+      		ColorPicker_targetInput.value = color;
+      		document.getElementById(ColorPicker_targetInput.id+\'_sample\').style.backgroundColor = color;
+      	}      	
+  			</script>
+  			';
+  		}
+  	} 
+  	function admin_page() {
+  		if(isset($this->erreur) and is_array($this->erreur)) {
+  			echo '<div id="message" class="error"><ul>';
+  			foreach($this->erreur as $msg) {
+  				echo '<li>'.$msg.'</li>';
+  			}
+  			echo '</ul></div>';
+  		} elseif(isset($this->msg)) {
+  			echo '<div id="message" class="updated fade"><p>'.$this->msg.'</p></div>';
+  		}
+  ?>
+  	<div class="wrap">
+  		<form action="" method="post">
+  		<h2><?php _e('Style Customizing', $this->domain); ?></h2>
+  			<div id="colorPickerDiv" style="z-index: 100;background:#eee;border:1px solid #ccc;position:absolute;visibility:hidden;"> </div>
+  			<table class="optiontable">
+  				<tbody>
+  					<tr>
+  						<th><?php _e('DashBar color', $this->domain);?></th>
+  						<td><input type="text" id="DashBar_bgcolor" name="DashBarStyle[bgcolor]" value="<?php echo $this->values['bgcolor'];?>" /> <input type="button" id="DashBar_bgcolor_sample" value="<?php _e('Color Picker',$this->domain);?>" /></td>
+  					</tr>
+  					<tr>
+  						<th><?php _e('Text Color', $this->domain);?></th>
+  						<td><input type="text" id="DashBar_color" name="DashBarStyle[color]" value="<?php echo $this->values['color'];?>" /> <input type="button" id="DashBar_color_sample" value="<?php _e('Color Picker',$this->domain);?>" /></td>
+  					</tr>					
+  					<tr>
+  						<th><?php _e('Highlighted element color', $this->domain);?></th>
+  						<td><input type="text" id="DashBar_fgcolor" name="DashBarStyle[fgcolor]" value="<?php echo $this->values['fgcolor'];?>" /> <input type="button" id="DashBar_fgcolor_sample" value="<?php _e('Color Picker',$this->domain);?>" /> </td>
+  					</tr>					
+  					<tr>
+  						<th><?php _e('Highlighted text color', $this->domain);?></th>
+  						<td><input type="text" id="DashBar_acolor" name="DashBarStyle[acolor]" value="<?php echo $this->values['acolor'];?>" /> <input type="button" id="DashBar_acolor_sample"  value="<?php _e('Color Picker',$this->domain);?>" /></td>
+  					</tr>					
+  					<tr>
+  						<th><?php _e('Text size', $this->domain);?></th>
+  						<td><input type="text" id="DashBar_height" name="DashBarStyle[height]" value="<?php echo $this->values['height'];?>" /></td>
+  					</tr>	
+  					<tr>
+  						<th><?php _e('Align', $this->domain);?></th>
+  						<td>
+  							<select id="DashBar_align" name="DashBarStyle[align]">
+  								<option value="left"<?php if($this->values['align'] == 'left') echo ' selected="selected"';?>><?php _e('left', $this->domain); ?></option>
+  								<option value="right"<?php if($this->values['align'] == 'right') echo ' selected="selected"';?>><?php _e('right', $this->domain); ?></option>
+  							</select>
+  						</td>
+  					</tr>
+  					<tr>
+  						<th><?php _e('Opacity', $this->domain);?></th>
+  						<td><input type="text" id="DashBar_opacity" name="DashBarStyle[opacity]" value="<?php echo $this->values['opacity'];?>" /></td>
+  					</tr>	
+  					
+  				</tbody>
+  			</table>
+  			<p class="submit"><input type="submit" name="DashBarAction" value="<?php _e('Change Style',$this->domain); ?>" /> <input type="submit" name="DashBarAction" value="<?php _e('Restore Default',$this->domain); ?>" /></p>
+  		</form>
+  	</div>
+  <?php	
+  	}  	 
+  }
+}
+$the_dashbar_variable = new DashBar();
+
+
+?>
